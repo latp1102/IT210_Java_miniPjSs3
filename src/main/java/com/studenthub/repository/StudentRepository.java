eb992ec7758f021653a5a6326c0ad56410715f7d
latp1102 <Latp1102@gmail.com>
2026-04-07 00:19:52 +0700

    Implement StudentRepository with sample data

--- src/main/java/com/studenthub/repository/StudentRepository.java
+++ src/main/java/com/studenthub/repository/StudentRepository.java
@@ -1,4 +1,22 @@
 package com.studenthub.repository;
-
+import org.example.studenthub.model.Student;
+import org.springframework.stereotype.Repository;
+@Repository
 public interface StudentRepository {
+    private final List<Student> students;
+    public StudentRepository() {
+        students = List.of(
+                new Student(1,"SV001","Nguyen Van C","CNTT",2021,3.8,"STUDYING"),
+                new Student(2,"SV002","Nguyen Van A","CNTT",2020,3.9,"GRADUATED"),
+                new Student(3,"SV003","Tran Thi B","ATTT",2022,3.2,"STUDYING"),
+                new Student(4,"SV004","Pham Van C","DTVT",2019,2.9,"SUSPENDED")
+        );
+    }
+    public List<Student> findAll() {
+        return students;
+    }
+    public Student findById(int id) {
+        return students.stream().filter(s -> s.getId() == id).findFirst().orElse(null);
+    }
 }
+
